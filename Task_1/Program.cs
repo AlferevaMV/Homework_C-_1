8e40fa1c5dd0eca65aaedf8df435e75f94a67724
Marina Alfereva <alferevamv@mail.ru>
2023-01-29 22:53:34 +0300

    Task1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,3 +2,18 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+
+Console.Write("Введи первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write($"max = {a}");
+}
+else
+{
+    Console.Write($"max = {b}");
+}
+    
